25158e3de5ff6578d9aed5ca51dc799d59e45140
Ivan Uravic <iuravic@gmail.com>
2017-06-18 22:39:40 +0200

    EventSimple constructor must take string

--- src/Core/Event/EventSimple.php
+++ src/Core/Event/EventSimple.php
@@ -5,8 +5,12 @@ use Duktig\Core\Event\EventAbstract;
 
 /**
  * This event class can be used to instantiate simple event objects on the run
- * which have no special state, and just use the event name as an ID.
+ * which have no special state, and just use the event name as their ID.
  */
 class EventSimple extends EventAbstract
 {
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+    }
 }
